8d09154ecdd7c1e6c26453a160c557e95ef5b388
Angel Cruz <136207924+angelcruz07@users.noreply.github.com>
2024-01-25 16:18:36 -0600

    Build

--- app/services/fetchData.jsx
+++ app/services/fetchData.jsx
@@ -3,8 +3,8 @@ export async function getData(type, query, limit = 6) {
 	const options = {
 		method: 'GET',
 		headers: {
-			'X-RapidAPI-Key': process.env.API_KEY,
-			'X-RapidAPI-Host': process.env.API_HOST
+			'X-RapidAPI-Key': '313129755emsh68f70976f320037p1b4615jsn802210f8c7f1',
+			'X-RapidAPI-Host': 'spotify23.p.rapidapi.com'
 		}
 	}
 	const res = await fetch(url, options)
